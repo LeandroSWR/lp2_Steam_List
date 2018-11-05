0002cc67e29f571527101613746af30f481c7f02
Leandro Brás <shadowgaming.7@gmail.com>
2018-11-05 12:25:53 +0000

    Added games file list

--- lp2_Steam_List/Program.cs
+++ lp2_Steam_List/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 namespace lp2_Steam_List {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine(Directory.GetCurrentDirectory());
         }
     }
 }
