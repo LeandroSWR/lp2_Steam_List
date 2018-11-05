e0d3a96323031761112138dd213c3c0a376f7fbb
Leandro Brás <shadowgaming.7@gmail.com>
2018-11-05 12:53:45 +0000

    Added experimental version of the Game class

---
+++ lp2_Steam_List/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lp2_Steam_List {
+    class Game {
+        int ID, RequiredAge, DLCCount, Metacritic, MovieCount, RecommendationCount,
+            ScreenshotCount, Owners, NumberOfPlayers, AchievementCount;
+        string Name, AboutText;
+        DateTime ReleaseDate;
+        bool ControllerSupport, PlatformWindows, PlatformLinux, PlatformMac, 
+            CategorySinglePlayer, CategoryMultiplayer, CategoryCoop, 
+            CategoryIncludeLevelEditor, CategoryVRSupport;
+        Uri SupportURL, HeaderImage, Website;
+
+        public Game(string stream) {
+            string[] parse = stream.Split(',');
+        }
+    }
+}
--- lp2_Steam_List/Program.cs
+++ lp2_Steam_List/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +7,7 @@ using System.Threading.Tasks;
 namespace lp2_Steam_List {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine(Directory.GetCurrentDirectory());
+
         }
     }
 }
