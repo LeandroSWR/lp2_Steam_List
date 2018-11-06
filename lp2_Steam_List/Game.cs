1f741a96e9d7b71f6f6410078da308b2250c8f09
Leandro Brás <shadowgaming.7@gmail.com>
2018-11-06 14:30:49 +0000

    Can now inicialize all the variables in the class Game

--- lp2_Steam_List/Game.cs
+++ lp2_Steam_List/Game.cs
@@ -18,6 +18,35 @@ namespace lp2_Steam_List {
 
         public Game(string stream) {
             string[] parse = stream.Split(',');
+            SaveData(parse);
+        }
+
+        private void SaveData(string[] parse) {
+            ID = Convert.ToInt32(parse[0]);
+            Name = parse[1];
+            ReleaseDate = Convert.ToDateTime(parse[2]);
+            RequiredAge = Convert.ToInt32(parse[3]);
+            DLCCount = Convert.ToInt32(parse[4]);
+            Metacritic = Convert.ToInt32(parse[5]);
+            MovieCount = Convert.ToInt32(parse[6]);
+            RecommendationCount = Convert.ToInt32(parse[7]);
+            ScreenshotCount = Convert.ToInt32(parse[8]);
+            Owners = Convert.ToInt32(parse[9]);
+            NumberOfPlayers = Convert.ToInt32(parse[10]);
+            AchievementCount = Convert.ToInt32(parse[11]);
+            ControllerSupport = Convert.ToBoolean(parse[12]);
+            PlatformWindows = Convert.ToBoolean(parse[13]);
+            PlatformLinux = Convert.ToBoolean(parse[14]);
+            PlatformMac = Convert.ToBoolean(parse[15]);
+            CategorySinglePlayer = Convert.ToBoolean(parse[16]);
+            CategoryMultiplayer = Convert.ToBoolean(parse[17]);
+            CategoryCoop = Convert.ToBoolean(parse[18]);
+            CategoryIncludeLevelEditor = Convert.ToBoolean(parse[19]);
+            CategoryVRSupport = Convert.ToBoolean(parse[20]);
+            SupportURL = new Uri(parse[21]);
+            AboutText = parse[22];
+            HeaderImage = new Uri(parse[22]);
+            Website = new Uri(parse[23]);
         }
     }
 }
