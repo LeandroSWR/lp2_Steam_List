e5a72a93883df75bfdbb617a336523fd2ef5e82d
fs000 <flaviosantos002010@hotmail.com>
2018-11-25 20:32:29 +0000

    Program Class fully commented

--- lp2_Steam_List/Program.cs
+++ lp2_Steam_List/Program.cs
@@ -5,12 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace lp2_Steam_List {
+
+    /// <summary>
+    /// The program starts here
+    /// </summary>
     class Program {
 
+        // The first key to ever be used by the dictionary
+        // Will be used by the the first option
         private static string DefaultKey { get; } = "ID";
-        
+
+        /// <summary>
+        /// The Main method from the Program Class
+        /// </summary>
+        /// <param name="args">Arguments accepted by the console</param>
         static void Main(string[] args) {
 
+            // Creates an instance of the Display class
             Display myDisplay = new Display(DefaultKey, args);
         }
     }
